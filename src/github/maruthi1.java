d75a563441510612f3ffb852d8d9d37874752da3
purna11 <g.purna89@gmail.com>
2018-01-27 12:24:10 +0530

    parvathi

--- src/github/maruthi1.java
+++ src/github/maruthi1.java
@@ -4,6 +4,8 @@ public class maruthi1 {
 	public static void main(String[] args) {
 		System.out.println(3);
 		System.out.println("commit");
+		System.out.println("parvathi");
+		System.out.println("parvathi2");
 	}
 
 }
